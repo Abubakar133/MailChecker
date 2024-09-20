0eccd296cc9f67bea483f9eeff6e1b8b8d2559fa
Abubakar133 <f201023@cfd.nu.eddu.pk>
2024-09-20 18:13:25 +0500

    akhb

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,7 +4,10 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  @Get()
+  getDefaultRoute() {
+    return 'Server is running successfully!';
+  }
   @Get()
   getHello(){
     return this.appService.getHello();
