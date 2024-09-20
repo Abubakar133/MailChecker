905dfea34d43e1fe768a2202fd05191f4176527c
Abubakar133 <f201023@cfd.nu.eddu.pk>
2024-09-20 18:40:20 +0500

    akhb

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './src/app.module';
-import dotenv from "dotenv";
+const dotenv = require('dotenv');
 
 async function bootstrap() {
 
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   dotenv.config();
-  
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
